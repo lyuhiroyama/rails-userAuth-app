1a5f8510c948c8dc0fe18454f03782eb3417dfe1
lyuhiroyama <lyuhiroyama@gmail.com>
2025-07-08 22:24:51 +0900

    feat: add 'create' and 'index' action to PostController
    - Ensured posts are created with current_user as author
    
    - Used strong parameters for 'create' action

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,25 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!, only: [ :new, :create ]
   def index
+    @posts = Post.all
   end
 
   def new
-    @post = Post.new
+    @post = current_user.posts.new
   end
 
   def create
+    @post = current_user.posts.new(post_params)
+    if @post.save
+      redirect_to posts_path # redirect to index view
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def post_params
+    params.expect(post: [ :title, :body ])
   end
 end
